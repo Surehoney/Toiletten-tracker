4043fd77dc6bae0062d219ee9371039db195d49d
Surehoney <mareike@konzeptberlin.com>
2025-03-15 11:59:21 +0100

    Version 2
    version 2

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Willkommen beim Toiletten-Tracker</h1>
+      <p>Hier kannst du deine Toilettengänge protokollieren.</p>
+    </div>
+  );
+}
